1f7d6bc8d9bbcf24bfbb19af7ad15572b247dbe5
Ben Kelly <benkell@iu.edu>
2020-09-11 23:38:11 -0400

    JavaScript Addition
    Added in JavaScript from the Tristen code

--- app/public/js/site.js
+++
@@ -1,9 +0,0 @@
-<script src='tablesort.min.js'></script>
-
-<!-- Include sort types you need -->
-<script src='tablesort.number.js'></script>
-<script src='tablesort.date.js'></script>
-
-<script>
-  new Tablesort(document.getElementById('table-id'));
-</script>
