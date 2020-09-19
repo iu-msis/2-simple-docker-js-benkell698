931d1237d8db96d28137894e2aa440c4a6035a4a
Ben Kelly <benkell@iu.edu>
2020-09-18 20:53:31 -0400

    Create profile.js

---
+++ app/public/js/profile.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+  el: '#userProfile',
+  data: {
+    userName: 'Hello Vue!'
+    userCountry:
+    userBirth
+    userAge
+    userEmail
+  },
+
+  created() {
+    this.fetchUser();
+  },
+
+  methods: {
+    fetchUser: function() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(data => {
+        var userData = data.results[0];
+        console.log(userData);
+        this.userName = userData.name.first + " " + userData.name.last;
+        this.userCountry = userData.country;
+        this.userBirth = userData.birthdate;
+        this.userAge = userData.age;
+        this.userEmail = userData.email;
+      })
+    }
+  }
+});
