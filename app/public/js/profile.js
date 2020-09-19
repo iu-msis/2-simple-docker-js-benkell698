65faa1a6308da535a9d3a1565586e84176f9c4cc
Ben Kelly <benkell@iu.edu>
2020-09-18 22:06:05 -0400

    Update profile.js

--- app/public/js/profile.js
+++ app/public/js/profile.js
@@ -21,7 +21,7 @@ var app = new Vue({
         console.log(userData);
         this.userName = userData.name.first + " " + userData.name.last;
         this.userCountry = userData.country;
-        this.userDob = userData.dob.date[0:9];
+        this.userDob = userData.dob.date;
         this.userAge = userData.age;
         this.userEmail = userData.email;
       })
