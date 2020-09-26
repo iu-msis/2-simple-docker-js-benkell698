02fafb482a7330f5b21aeb1f0dea24d06bbe5657
Ben Kelly <benkell@iu.edu>
2020-09-25 23:46:30 -0400

    Update profile.js

--- app/public/js/profile.js
+++ app/public/js/profile.js
@@ -27,6 +27,21 @@ var app = new Vue({
         this.userAge = userData.dob.age;
         this.userEmail = userData.email;
       })
+    },
+    newUser: function() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(data => {
+        var userData = data.results[0];
+        console.log(userData);
+        this.userImage = userData.picture.large;
+        this.userName = userData.name.first + " " + userData.name.last;
+        this.userCountry = userData.location.country;
+        this.userDob = userData.dob.date[5]+ userData.dob.date[6] + '/' + userData.dob.date[8] + userData.dob.date[9] + '/' + userData.dob.date[0] + userData.dob.date[1] + userData.dob.date[2] + userData.dob.date[3];
+        this.userAge = userData.dob.age;
+        this.userEmail = userData.email;
+      })
+    },
     }
   }
 });
