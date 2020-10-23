9dc8bf8c9e96de0af4ff41896452fd5ce82214ba
Ben Kelly <benkell@iu.edu>
2020-10-23 15:07:48 -0400

    Update create.php

--- app/public/api/comments/create.php
+++ app/public/api/comments/create.php
@@ -12,12 +12,12 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'INSERT INTO Comments (id, commentText)
-  VALUES (?, ?)'
+  'INSERT INTO Comments (commentText)
+  VALUES (?)'
 );
 
 $stmt->execute([
-  $_POST['id'],
+  // $_POST['commentId']
   $_POST['commentText']
 ]);
 
@@ -28,5 +28,5 @@ $stmt->execute([
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../comments/?id=' . $id);
+header('Location: ../comments/?id=' . $pk);
 // header('Location: ../comments/');
